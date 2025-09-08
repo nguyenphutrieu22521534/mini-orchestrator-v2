c2801bce206f481fd9c69d3ac6a30a079929b1d4
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2025-09-08 16:02:46 +0700

    .

--- core/command_registry.py
+++ core/command_registry.py
@@ -3,9 +3,7 @@ class Registry:
         self.commands = {}
         self.pending_args = {}
 
-    def register(self, name: str, help_text: str = ""):
-        """Decorator để đăng ký command"""
-
+    def register_decorator(self, name: str, help_text: str = ""):
         def decorator(func):
             self.commands[name] = {
                 'func': func,
@@ -17,12 +15,19 @@ class Registry:
                 self.commands[name]['args'].extend(self.pending_args[name])
                 del self.pending_args[name]
             return func
-
         return decorator
 
-    def add_argument(self, command_name: str, *args, **kwargs):
-        """Thêm argument cho command cụ thể"""
+    def register(self, name: str, func, help_text: str = ""):
+        self.commands[name] = {
+            'func': func,
+            'help': help_text,
+            'args': []
+        }
+        if name in self.pending_args:
+            self.commands[name]['args'].extend(self.pending_args[name])
+            del self.pending_args[name]
 
+    def add_argument_decorator(self, command_name: str, *args, **kwargs):
         def decorator(func):
             if command_name in self.commands:
                 self.commands[command_name]['args'].append((args, kwargs))
@@ -32,8 +37,15 @@ class Registry:
                     self.pending_args[command_name] = []
                 self.pending_args[command_name].append((args, kwargs))
             return func
-
         return decorator
 
+    def add_argument(self, command_name: str, *args, **kwargs):
+        """Thêm argument cho command mà không cần decorator"""
+        if command_name in self.commands:
+            self.commands[command_name]['args'].append((args, kwargs))
+        else:
+            if command_name not in self.pending_args:
+                self.pending_args[command_name] = []
+            self.pending_args[command_name].append((args, kwargs))
 # Tạo instance mặc định để sử dụng trong toàn bộ ứng dụng
 registry = Registry()
--- main.py
+++ main.py
@@ -4,21 +4,29 @@ from core.command_registry import registry
 from features.ingest import LogParser
 from features.prime import PrimeCalculator
 
-@registry.register('ingest', 'Log parsing feature')
-@registry.add_argument('ingest', '--path', type=str, required=True, help='Path to log file')
-@registry.add_argument('ingest', '--worker', type=int, default=2, help='Number of workers (default: 2)')
-@registry.add_argument('ingest', '--mode', choices=['threading', 'process'], default='threading',
+#@registry.register('ingest', 'Log parsing feature')
+@registry.add_argument_decorator('ingest', '--path', type=str, required=True, help='Path to log file')
+@registry.add_argument_decorator('ingest', '--worker', type=int, default=2, help='Number of workers (default: 2)')
+@registry.add_argument_decorator('ingest', '--mode', choices=['threading', 'process'], default='threading',
                      help='Execution mode: threading or process (default: threading)')
 def run_ingest(args: argparse.Namespace) -> None:
+
     log_parser = LogParser(args.path, args.worker, args.mode)
     results = log_parser.parse_logs()
     log_parser.display_results(results)
 
+@registry.register_decorator('prime', 'Prime calculations feature')
 def run_prime(args: argparse.Namespace) -> None:
     prime_calc = PrimeCalculator(args.worker, args.mode)
     prime_calc.calculate_primes()
 
 def main():
+    registry.register('ingest', run_ingest, 'Log parsing feature')
+
+    registry.add_argument('prime', '--worker', type=int, default=2, help='Number of workers (default: 2)')
+    registry.add_argument('prime', '--mode', choices=['threading', 'process'], default='threading',
+                     help='Execution mode: threading or process (default: threading)')
+
     parser = argparse.ArgumentParser(
         description="Mini Orchestrator - Log parsing and Prime calculations",
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -26,20 +34,11 @@ def main():
 
     subparsers = parser.add_subparsers(dest='command', required=True)
 
-    # Tự động tạo subparser cho các command trong registry (ở đây chỉ có ingest)
     for cmd_name, cmd_info in registry.commands.items():
         cmd_parser = subparsers.add_parser(cmd_name, help=cmd_info['help'])
         for arg_args, arg_kwargs in cmd_info['args']:
             cmd_parser.add_argument(*arg_args, **arg_kwargs)
         cmd_parser.set_defaults(func=cmd_info['func'])
-
-    # Đăng ký thủ công cho prime command
-    
-    prime_parser = subparsers.add_parser('prime', help='Prime calculations feature')
-    prime_parser.add_argument('--worker', type=int, default=2, help='Number of workers (default: 2)')
-    prime_parser.add_argument('--mode', choices=['threading', 'process'], default='threading',
-                          help='Execution mode: threading or process (default: threading)')
-    prime_parser.set_defaults(func=run_prime)
 
     args = parser.parse_args()
     if hasattr(args, 'func'):
